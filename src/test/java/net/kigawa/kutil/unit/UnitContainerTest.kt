b2179d708b019567dbcd20871cdce859891a1350
kigawa <kigawa.8390@gmail.com>
2022-11-12 16:00:17 +0900

    support async and fix test

--- src/test/java/net/kigawa/kutil/unit/UnitContainerTest.kt
+++ src/test/java/net/kigawa/kutil/unit/UnitContainerTest.kt
@@ -8,40 +8,53 @@ import net.kigawa.kutil.unit.dummy.Unit4
 import net.kigawa.kutil.unit.dummy.parent.AbstractUnit
 import net.kigawa.kutil.unit.dummy.parent.AllUnitInterface
 import net.kigawa.kutil.unit.dummy.parent.UnitInterface1
+import net.kigawa.kutil.unit.runtimeexception.NoFoundUnitException
+import net.kigawa.kutil.unit.runtimeexception.NoSingleUnitException
 import net.kigawa.kutil.unit.runtimeexception.RuntimeUnitException
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
 internal class UnitContainerTest : Assertions() {
+
+
     @Test
-    fun testGetUnit() {
-        val con = UnitContainer.create()
-        try {
-            con.registerUnits(ClassList.create(javaClass))
-            con.initUnits()
-        } catch (e: UnitException) {
-            e.printStackTrace()
-            throw RuntimeException(e)
-        }
-        val executor = Executors.newCachedThreadPool()
-        con.addUnit(executor)
-
-        con.getIdentifies()
-
+    fun testGet() {
         assertNotNull(con.getUnit(Unit4::class.java))
         assertNotNull(con.getUnit(Unit1::class.java))
         assertNotNull(con.getUnit(UnitInterface1::class.java))
         assertNotNull(con.getUnit(Unit2::class.java))
         assertNotNull(con.getUnit(AbstractUnit::class.java))
         assertNotNull(con.getUnit(ExecutorService::class.java))
-        assertThrows(RuntimeUnitException::class.java) { con.getUnit(AllUnitInterface::class.java) }
-        executor.shutdown()
+        assertThrows(NoSingleUnitException::class.java) { con.getUnit(AllUnitInterface::class.java) }
     }
 
     @Test
     fun test() {
         assertFalse(Unit1::class.java == Unit2::class.java)
+    }
+
+    companion object {
+        private val executor = Executors.newCachedThreadPool()
+        private val con: UnitContainer = UnitContainer.create()
+
+        @JvmStatic
+        @BeforeAll
+        fun beforeAll() {
+            con.addUnit(executor)
+            con.executor = executor::execute
+            con.getIdentifies()
+            con.registerUnits(ClassList.create(UnitContainerTest::class.java))
+            con.initUnits()
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun afterAll() {
+            executor.shutdown()
+        }
     }
 }
