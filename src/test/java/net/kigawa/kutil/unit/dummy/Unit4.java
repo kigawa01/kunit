f1f4594c8e2ea4aa3a4a303b6cc499db5f62e12f
kigawa <kigawa.8390@gmail.com>
2022-11-13 15:18:02 +0900

    add get units test

--- src/test/java/net/kigawa/kutil/unit/dummy/Unit4.java
+++ src/test/java/net/kigawa/kutil/unit/dummy/Unit4.java
@@ -3,10 +3,8 @@ package net.kigawa.kutil.unit.dummy;
 import net.kigawa.kutil.unit.annotation.*;
 import net.kigawa.kutil.unit.dummy.parent.*;
 
-@Dependency(value = Unit1.class)
-@Dependency(value = Unit2.class)
 @Unit
-public class Unit4 extends AbstractUnit implements UnitInterface1, AllUnitInterface
+public class Unit4 extends AbstractUnit implements UnitInterface1, UnitOneToFourInterface
 {
     public Unit4()
     {
