61cd2db80eefc7e65835da0cfee28f1a0222f105
kigawa <kigawa.8390@gmail.com>
2023-01-10 19:18:06 +0900

    add

---
+++ src/main/java/net/kigawa/kutil/unit/extension/registrar/InstanceListRegistrar.kt
@@ -0,0 +1,22 @@
+package net.kigawa.kutil.unit.extension.registrar
+
+import net.kigawa.kutil.unit.annotation.getter.LateInit
+import net.kigawa.kutil.unit.api.component.*
+import net.kigawa.kutil.unit.component.UnitIdentify
+import net.kigawa.kutil.unit.extension.registeroption.InstanceOption
+import net.kigawa.kutil.unit.extension.registeroption.RegisterOptions
+
+@LateInit
+open class InstanceListRegistrar(
+  getterComponent: UnitGetterComponent, databaseComponent: UnitDatabaseComponent,
+  container: UnitContainer,
+):
+  AbstractRegister(getterComponent, databaseComponent, container) {
+  fun register(identifies: List<Any>) {
+    identifies.map {
+      registerTask(UnitIdentify(it.javaClass, null), RegisterOptions(InstanceOption(it)))
+    }.forEach {
+      it()
+    }
+  }
+}
