679ecbaac939d4e6ed90f9ae4f16a9784b23cbc4
kigawa <kigawa.8390@gmail.com>
2023-02-09 20:50:03 +0900

    add test

--- kutil-unit/src/test/java/net/kigawa/kutil/unit/UnitContainerTest.kt
+++ kutil-unit/src/test/java/net/kigawa/kutil/unit/UnitContainerTest.kt
@@ -19,6 +19,12 @@ import java.util.concurrent.Executors
 @RunWith(JUnit4::class)
 internal class UnitContainerTest: AbstractTest() {
   @Test
+  fun testParent() {
+    val container = UnitContainer.create(con)
+    assertSize(1, container.getUnitList(ResourceRegistrar::class.java))
+  }
+  
+  @Test
   fun testGet() {
     assertNotNull(con.getUnit(Unit4::class.java))
     assertNotNull(con.getUnit(Unit1::class.java))
