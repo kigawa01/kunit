556c77221ae70c7264065d4cb9ed5026b6862b7d
kigawa <kigawa.8390@gmail.com>
2023-03-02 21:32:44 +0900

    add instanceRegistrar waning

--- kutil-unit-impl/src/main/java/net/kigawa/kutil/unit/registrar/InstanceRegistrarImpl.kt
+++ kutil-unit-impl/src/main/java/net/kigawa/kutil/unit/registrar/InstanceRegistrarImpl.kt
@@ -1,13 +1,19 @@
 package net.kigawa.kutil.unit.registrar
 
 import net.kigawa.kutil.unitapi.annotation.getter.LateInit
+import net.kigawa.kutil.unitapi.component.UnitLoggerComponent
 import net.kigawa.kutil.unitapi.options.RegisterOptions
 import net.kigawa.kutil.unitapi.options.RegistrarInstanceOption
 import net.kigawa.kutil.unitapi.registrar.InstanceRegistrar
+import java.util.logging.Level
 
 @LateInit
-class InstanceRegistrarImpl(private val classRegistrar: ClassRegistrarImpl): InstanceRegistrar {
+class InstanceRegistrarImpl(
+  private val classRegistrar: ClassRegistrarImpl,
+  private val loggerComponent: UnitLoggerComponent,
+): InstanceRegistrar {
   override fun register(instance: Any, name: String?) {
+    loggerComponent.log(Level.WARNING, "class registered with instance registrar", null, instance)
     classRegistrar.register(instance.javaClass, name, RegisterOptions(RegistrarInstanceOption(instance)))
   }
   
