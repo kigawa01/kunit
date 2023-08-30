705d2ce2fc7bbee7009bb7bfefe9003a94c834eb
kigawa <kigawa.8390@gmail.com>
2023-08-31 00:23:32 +0900

    add: ResourceRegistrar extension

--- kutil-unit-impl/src/main/java/net/kigawa/kutil/unitimpl/registrar/ResourceRegistrarImpl.kt
+++ kutil-unit-impl/src/main/java/net/kigawa/kutil/unitimpl/registrar/ResourceRegistrarImpl.kt
@@ -1,10 +1,11 @@
 package net.kigawa.kutil.unitimpl.registrar
 
 import net.kigawa.kutil.unitapi.annotation.getter.LateInit
-import net.kigawa.kutil.unitapi.component.*
+import net.kigawa.kutil.unitapi.component.UnitLoggerComponent
 import net.kigawa.kutil.unitapi.exception.UnitException
 import net.kigawa.kutil.unitapi.extention.Message
 import net.kigawa.kutil.unitapi.registrar.ResourceRegistrar
+import net.kigawa.kutil.unitapi.registrar.SelectionRegistrar
 import net.kigawa.kutil.unitimpl.util.LocaleBuilder
 import java.io.File
 import java.net.JarURLConnection
@@ -15,27 +16,56 @@ import java.util.logging.Level
 @LateInit
 class ResourceRegistrarImpl(
   private val loggerComponent: UnitLoggerComponent,
-  getterComponent: UnitStoreComponent,
-  databaseComponent: UnitDatabaseComponent,
-  container: UnitContainer,
-): SelectionRegistrarImpl(getterComponent, databaseComponent, container), ResourceRegistrar {
+  private val selectionRegistrar: SelectionRegistrar,
+): ResourceRegistrar {
   companion object {
     const val JAR_PROTOCOL = "jar"
     const val FILE_PROTOCOL = "file"
   }
   
-  override fun register(classLoader: ClassLoader, packageName: String) {
+  override fun register(classLoader: ClassLoader, packageName: String, selectionRegistrar: SelectionRegistrar) {
     val packageDir = packageName.replace('.', '/')
     classLoader.getResources(packageDir).asIterator().forEach {
       when (it.protocol) {
-        JAR_PROTOCOL ->registerJar(classLoader, it, packageName)
-        FILE_PROTOCOL->registerFile(classLoader, it, packageName)
+        JAR_PROTOCOL ->registerJar(classLoader, it, packageName, selectionRegistrar)
+        FILE_PROTOCOL->registerFile(classLoader, it, packageName, selectionRegistrar)
         else         ->throw UnitException("could not support resource protocol")
       }
     }
   }
   
-  private fun registerJar(classLoader: ClassLoader, resource: URL, packageName: String) {
+  override fun register(classLoader: ClassLoader, packageName: String) {
+    register(classLoader, packageName, selectionRegistrar)
+  }
+  
+  private fun tryRegister(
+    classLoader: ClassLoader,
+    className: String,
+    selectionRegistrar: SelectionRegistrar,
+    errInfo: List<Any>,
+  ): (()->Unit)? {
+    return try {
+      val unitClass = classLoader.loadClass(className)
+      selectionRegistrar.selectRegister(unitClass)
+    } catch (e: Throwable) {
+      loggerComponent.log(
+        Message(
+          Level.WARNING,
+          LocaleBuilder("could not register class").toString(),
+          listOf(e),
+          errInfo
+        )
+      )
+      null
+    }
+  }
+  
+  private fun registerJar(
+    classLoader: ClassLoader,
+    resource: URL,
+    packageName: String,
+    selectionRegistrar: SelectionRegistrar,
+  ) {
     if (JAR_PROTOCOL != resource.protocol) throw RuntimeException("could not support file type")
     
     (resource.openConnection() as JarURLConnection).jarFile.use {jarFile->
@@ -44,54 +74,38 @@ class ResourceRegistrarImpl(
         if (!name.startsWith(packageName.replace('.', '/'))) return@map null
         if (!name.endsWith(".class")) return@map null
         name = name.replace('/', '.').replace(".class$".toRegex(), "")
-        try {
-          val unitClass = classLoader.loadClass(name)
-          selectRegister(unitClass)
-        } catch (e: Throwable) {
-          loggerComponent.log(
-            Message(
-              Level.WARNING,
-              LocaleBuilder("could not register class").toString(),
-              listOf(e),
-              listOf(name, classLoader, resource)
-            )
-          )
-          null
-        }
+        tryRegister(classLoader, name, selectionRegistrar, listOf(name, classLoader, resource))
       }
     }.forEach {it?.invoke()}
   }
   
-  private fun registerFile(classLoader: ClassLoader, resource: URL, packageName: String) {
+  private fun registerFile(
+    classLoader: ClassLoader,
+    resource: URL,
+    packageName: String,
+    selectionRegistrar: SelectionRegistrar,
+  ) {
     if (FILE_PROTOCOL != resource.protocol) throw RuntimeException("could not support file type")
-    loadUnit(classLoader, File(resource.file), packageName).forEach {it?.invoke()}
+    loadUnit(classLoader, File(resource.file), packageName, selectionRegistrar).forEach {it?.invoke()}
   }
   
-  private fun loadUnit(classLoader: ClassLoader, dir: File, packageName: String): List<(()->Unit)?> {
+  private fun loadUnit(
+    classLoader: ClassLoader,
+    dir: File,
+    packageName: String,
+    selectionRegistrar: SelectionRegistrar,
+  ): List<(()->Unit)?> {
     val files = dir.listFiles() ?: throw UnitException("cold not load unit files")
     return files.map {file->
       if (file.isDirectory) {
-        val list = loadUnit(classLoader, file, packageName + "." + file.name)
+        val list = loadUnit(classLoader, file, packageName + "." + file.name, selectionRegistrar)
         return@map {list.forEach {it?.invoke()}}
       }
       if (!file.name.endsWith(".class")) return@map null
       var name = file.name
       name = name.replace(".class$".toRegex(), "")
       name = "$packageName.$name"
-      try {
-        val unitClass = classLoader.loadClass(name)
-        selectRegister(unitClass)
-      } catch (e: Throwable) {
-        loggerComponent.log(
-          Message(
-            Level.WARNING,
-            LocaleBuilder("could not register class").toString(),
-            listOf(e),
-            listOf(name, classLoader, dir)
-          )
-        )
-        null
-      }
+      tryRegister(classLoader, name, selectionRegistrar, listOf(name, classLoader, dir))
     }
   }
 }
