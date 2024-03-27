9d5744ab7d55ec7675f15a5858138592ea2464ef
kigawa <kigawa.8390@gmail.com>
2024-03-27 13:23:31 +0900

    add lint

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,6 +5,7 @@
 plugins {
   // Support convention plugins written in Kotlin. Convention plugins are build scripts in 'src/main' that automatically become available as plugins in the main build.
   `kotlin-dsl`
+  id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
 }
 dependencies{
   implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.23")
